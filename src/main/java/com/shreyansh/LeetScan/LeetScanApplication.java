d223075182ae56b5534bf7001804a042a7330db6
Shreyansh <shreyanshloop07@gmail.com>
2026-08-04 22:51:11 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/shreyansh/LeetScan/LeetScanApplication.java
@@ -0,0 +1,17 @@
+package com.shreyansh.LeetScan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class LeetScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetScanApplication.class, args);
+	}
+	    
+     
+  }
+
